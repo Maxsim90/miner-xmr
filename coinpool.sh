f2047478714f2f2ef61637a7ef5bf7a9f3d1a7e8
loaman123 <39140414+loaman123@users.noreply.github.com>
2018-09-20 16:35:08 +0300

    Update coinpool.sh

--- coinpool.sh
+++ coinpool.sh
@@ -3,26 +3,26 @@ if [ "$(whoami)" != "root" ]; then
         echo -e "\033[0;31mSorry, you are not root. Please use sudo option\033[0m"
         exit 1
 fi
-
-WALLET=
+WALLET=0000000000000000000
 ID="$(hostname)"
 PASSWORD=x
 THREADS="$(nproc --all)"
 
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
-	sudo sysctl -p
+sudo sysctl -p
 echo -e '\033[0;32m##### Installing updates and install soft...\033[0m'
-	sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
 sleep 2
-	rm -rf /tmp/miner
+rm -rf /tmp/miner
 cd /tmp && mkdir miner
-	git clone https://github.com/loaman123/miner-xmr.git /tmp/miner
+git clone https://github.com/loaman123/miner-xmr.git /tmp/miner
 cd /tmp/miner
-	chmod +x /tmp/miner/xmrig
+chmod +x /tmp/miner/xmrig
 sleep 1
-	cp /tmp/miner/xmrig /usr/bin/
+cp /tmp/miner/xmrig /usr/bin/
 sleep 1
-	xmrig -o coinpool.ws:5555 -u $WALLET --pass=$PASSWORD --threads=$THREADS -B -l /tmp/miner/coinpool.log --donate-level=1 --print-time=10 --variant 1 -k 
+#xmrig -c /tmp/miner/config.json
+xmrig -o coinpool.ws:5555 -u $WALLET.$ID --pass=$PASSWORD --rig-id=$ID --threads=$THREADS -B -l /tmp/miner/coinpool.log --donate-level=1 --cpu-priority=5 --max-cpu-usage=80 --print-time=10 --variant=1 -k 
 echo -e '\033[0;32m##### Miner started \033[0m'
 echo -e '\033[0;32m##### Watch: \033[0m'
-echo -e '\033[0;32m##### tail -f /tmp/miner/xmrig.log \033[0m'
+echo -e '\033[0;32m##### tail -f /tmp/miner/qrl.log \033[0m'
